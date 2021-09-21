84481e4d4d9a0715029b13cc3a156eecab79c9ed
Flora Davies <daviesfgo@gmail.com>
2021-09-21 15:51:34 +0100

    Changed comments

--- public/src/canvas.js
+++ public/src/canvas.js
@@ -1,23 +1,24 @@
-var canvas = document.getElementById("canvas"); //takes the HTML canvas element
-var ctx = canvas.getContext("2d"); // the elements context - what we will draw onto (could call this anything like context docs call it ctx)
+var canvas = document.getElementById("canvas"); // finds the HTML element of canvas
+var ctx = canvas.getContext("2d"); // what we will draw onto (could call this anything I like, context docs call it ctx)
 var balls = new Array();
 
+// on click function that takes the x and y coordinate of the mouse click
 canvas.addEventListener("click", function (e) {
   balls.push(new Ball(e.clientX, e.clientY));
 });
 
-function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function reDrawLoop() {
   let height = document.documentElement.clientHeight;
   let width = document.documentElement.clientWidth;
-  window.requestAnimationFrame(loop); // resizing the canvas properly so that the images are not skewed on it Tells the browser that you wish to perform an animation and requests that the browser call a specified function to update an animation before the next repaint.
-  ctx.canvas.height = height;
+  ctx.canvas.height = height; // resizing the canvas properly so that the images are not skewed
   ctx.canvas.width = width;
 
   balls.forEach((ball) => {
     ball.draw();
     ball.updatePosition(width, height);
   });
+
+  window.requestAnimationFrame(reDrawLoop); // Tells the browser that you wish to perform an animation and requests that the browser call a specified function to update an animation before the next repaint.
 }
 
-loop();
+reDrawLoop();
